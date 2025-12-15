aac5be9a63f3b2fa3bffd80496c510707232147f
kimhimit-code <kimhimit@gmail.com>
2025-12-15 17:30:30 +0700

    Create jscript.js

---
+++ jscript.js
@@ -0,0 +1,32 @@
+function displayImg(img){
+
+    document.getElementById("img-big").src=img;
+
+
+}
+
+
+
+
+
+// function displayImg1(){
+//     document.getElementById("img-big").src="image/ALl_product/5.jpg";
+// }
+
+// function displayImg2(){
+//     document.getElementById("img-big").src="image/ALl_product/6.jpg";
+// }
+
+// function displayImg3(){
+//     document.getElementById("img-big").src="image/ALl_product/7.jpg";
+// }
+
+// function displayImg4(){
+//     document.getElementById("img-big").src="image/ALl_product/8.jpg";
+// }
+
+// function displayImg5(){
+//     document.getElementById("img-big").src="image/ALl_product/9.jpg";
+// }
+    
+
